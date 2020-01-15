913c3f0207825c78b3c7abe1b3b23ba2f3d6d3ce
Bailey Day <bailey.j.day@gmail.com>
2020-01-15 15:50:31 -0500

    A python script that converts Celsius to Farenheit from a user input.

---
+++ degree_conversion.py
@@ -0,0 +1,7 @@
+# A script that converts Celsius to Farenheit
+
+celsius = int(input("What is the temperature in Celsius?"))
+
+farenheit = (celsius * 1.8) + 32
+
+print("The temperature is " + str(farenheit) + " degrees Farenheit")
