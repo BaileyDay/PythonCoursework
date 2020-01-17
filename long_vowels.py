3eb85bca02388e77c4cfc9cc7dffcf09548efecf
Bailey Day <bailey.j.day@gmail.com>
2020-01-17 09:27:41 -0500

    Fixed syntax and removed some extra code.

--- long_vowels.py
+++ long_vowels.py
@@ -7,16 +7,13 @@ vowels = "aeiou"
 
 new_string = " "
 
-index = 0
-
 for i in range(len(user)):
     if user[i] == user[i - 1] and user[i] in vowels:
         new_string += user[i] * 4
     else:
         new_string += user[i]
-    i += 1 
 print(new_string)
-    
+
 
 
         
